305a1d7d8e42ca4f3a11c180417c6097d93762d7
Cristián Gabriel Sánchez <cristian.g.sanchez@gmail.com>
2020-10-20 14:28:47 -0300

    tarea matplotlib

---
+++ tareas/12_matplotlib.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.6.0
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown]
+# ## Ejercicio 1
+#
+# Graficar las siguientes funciones en los intervalos dados:
+#
+# 1. $f(x) = \cos(x^2)$ en $[-3\pi,3\pi]$
+# 1. $f(x) = x\sin(1/x)$ en $[-1,1]$ (se necesita un poco de ingeniería para lidiar con las infinitas discontinuidades)
+# 1. $J_n(x)$ con $n=1,2,3,\ldots,5$ en el intervalo $[0,10]$ donde $J_n$ es la función de Bessel del primer tipo.
+#
+
+# %%
+
+# %% [markdown]
+# ## Ejercicio 2
+#
+# Dada la función compleja de variable real:
+#
+# $\mu(t) = e^{it} + \frac{1}{2}e^{6it} + \frac{i}{3}e^{-14it}$,
+#
+# graficar la curva paramétrica dada por $(\Re \mu(t),\Im \mu(t))$ con $t\in[0,2\pi]$.
+#
+# Referencia: Creating symmetry : the artful mathematics of wallpaper patterns, Farris, Frank A, Princeton University Press, 2015
+
+# %%
+
+# %%
+## Ejercicio S1
+
+
+
+# %%
